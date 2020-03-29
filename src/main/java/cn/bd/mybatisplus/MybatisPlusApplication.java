5d79e060cb55081b81899ef339a7e6f6d29eb1e8
Mr丶Cps <peishen.cai@foxmail.com>
2020-03-29 14:40:06 +0800

    初始化

---
+++ src/main/java/cn/bd/mybatisplus/MybatisPlusApplication.java
@@ -0,0 +1,16 @@
+package cn.bd.mybatisplus;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class MybatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusApplication.class, args);
+    }
+
+}
